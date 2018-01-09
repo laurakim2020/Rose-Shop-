d39ee61ea6292c6c20bdd415eaf2153bbcedcaab
laurakim2020 <31958630+laurakim2020@users.noreply.github.com>
2018-01-09 00:01:25 -0500

    Add files via upload

---
+++ Rose.py
@@ -0,0 +1,31 @@
+print ("Welcome to Le Fleur De Rose") 
+def numflower(num):
+    if num == 0:
+        return "Rose Eden"
+    elif num == 1:
+        return "Golden Celebration"
+    elif num == 2:
+        return "Damask Rose"
+    elif num == 3:
+        return "Beach Rose"
+    elif num == 4:
+        return "Dog-Rose"
+    else:
+        return "Please enter the rose you would like according to its number"
+print(numflower(int(input("Please enter the rose you would like according to its number"))))
+
+items = int(input("How many roses do you want?"))
+if items >= 12:
+    print("No one needs that many flowers")
+else:
+    print("Nice and Simple")
+
+question = input("Would you like to change the color")
+if question == "yes":
+    print ("Great")
+else:
+    print("Nice")
+    
+Color = input("What color would you like your roses to be: Lavender, Maroon, Gold, Yellow")
+Box = input("Choose a box color: Black, White, Grey")
+
