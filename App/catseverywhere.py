3cd090c65fd16ed48b7156b8972363528e91fde7
laurakim2020 <laurakim2020@gmail.com>
2018-06-03 18:10:21 -0400

    Update Version

---
+++ App/catseverywhere.py
@@ -0,0 +1,18 @@
+from flask import request, redirect
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    author = "Me"
+    name = "You"
+    return render_template('index.html', author=author, name=name)
+
+@app.route('/signup', methods = ['POST'])
+def signup():
+    email = request.form['email']
+    print("The email address is '" + email + "'")
+    return redirect('/')
+
+if __name__ == "__main__":
+    app.run()
